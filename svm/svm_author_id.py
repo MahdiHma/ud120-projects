369da1849401b4d8a585ea1851952ad1dccbdfe5
mahdihma <mahdi.haji@yahoo.com>
2020-07-23 23:05:04 +0430

    svm miniproject started

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,12 +7,14 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +22,16 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+t0 = time()
+clf = SVC(kernel='linear')
 
+clf.fit(features_train, labels_train)
+print "fit time:", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "fit time:", round(time() - t0, 3), "s"
+print accuracy_score(pred, labels_test)
 #########################################################
-
-
